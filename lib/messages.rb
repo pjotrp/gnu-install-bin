fd8e72bdefbe7b6ea1d0a62039658fd5f2921e80
Pjotr Prins <pjotr.public01@thebird.nl>
2016-12-24 05:37:03 +0000

    Add debug layer and handle exit status of shell call

--- lib/messages.rb
+++ lib/messages.rb
@@ -26,12 +26,17 @@ module Messages
   end
 
   def shell cmd
-    debug "RUN "+cmd
-    if @options[:debug] or @options[:verbose]
-      `#{cmd}`.strip
-    else
-      `#{cmd} 2>/dev/null`.strip
+    debug "RUN "+cmd if @options[:debug2]
+    res = if @options[:debug] or @options[:verbose]
+            `#{cmd}`.strip
+          else
+            `#{cmd} 2>/dev/null`.strip
+          end
+    retval = $?.exitstatus >> 8
+    if retval != 0
+      error "exit status #{retval} for #{cmd}"
     end
+    res
   end
 
 end
